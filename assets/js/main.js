e3a7fda33e3fac0de226a0523e86f73409547508
entonibaba0720 <entonibaba0720@gmail.com>
2018-06-25 17:04:39 +0200

    little changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,5 +3,9 @@ $(document).ready(function() {
         $('#nav').toggleClass('active');
     });
 
+    $('nav ul li a').click(function() {
+        $('nav ul li a').removeClass('active');
+        $(this).addClass('active');
 
+    });
 });
